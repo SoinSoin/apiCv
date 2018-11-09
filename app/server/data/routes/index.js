01db6c3882455e63b70b064715a84fc1c26f00a6
SoinSoin <vincent.retore@gmail.com>
2018-11-09 13:15:35 +0100

    resolve error stash

--- app/server/data/routes/index.js
+++ app/server/data/routes/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
-const app = express(); 
+const app = express();
 app.use('/me', require('./me'));
 app.use('/page', require('./page'));
 app.use('/api', require('./api'));
+app.use('', (req, res, next) => {
+    if (res.status(404))
+        res.send('Sorry for 404 error (・・；)')
+    // res.redirect('/error');
+})
 
 module.exports = app;
